620f94155e747bef52115ad4b3f6756d10ea8b03
niman991 <82784468+niman991@users.noreply.github.com>
2021-07-03 02:20:55 +0200

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -26,7 +26,7 @@ const config: MenuEntry[] = [
     href: '/farms',
   },
   {
-    label: 'Nests',
+    label: 'Pools',
     icon: 'PoolIcon',
     href: '/nests',
   },
@@ -46,28 +46,6 @@ const config: MenuEntry[] = [
   //   href: '/nft',
   // },
   {
-    label: 'Info',
-    icon: 'InfoIcon',
-    items: [
-      {
-        label: 'PancakeSwap',
-        href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
-      },
-      {
-        label: 'CoinGecko',
-        href: 'https://www.coingecko.com/en/coins/goose-finance',
-      },
-      {
-        label: 'CoinMarketCap',
-        href: 'https://coinmarketcap.com/currencies/goose-finance/',
-      },
-      {
-        label: 'AstroTools',
-        href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
-      },
-    ],
-  },
-  {
     label: 'More',
     icon: 'MoreIcon',
     items: [
@@ -85,20 +63,20 @@ const config: MenuEntry[] = [
       },
     ],
   },
+    {
+    label: 'GitHub',
+    icon: 'GooseIcon',
+    href: 'https://github.com/',
+  },
   {
     label: 'Partnerships/IFO',
     icon: 'GooseIcon',
     href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
   },
   {
-    label: 'Audit by Hacken',
+    label: 'Audit coming soon',
     icon: 'AuditIcon',
-    href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
-  },
-  {
-    label: 'Audit by CertiK',
-    icon: 'AuditIcon',
-    href: 'https://certik.org/projects/goose-finance',
+    href: '/',
   },
 ]
 
