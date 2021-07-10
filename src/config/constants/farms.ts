4aa53549fe259d796279990304dc0bc8bbffb1ad
tuejul6 <87008121+tuejul6@users.noreply.github.com>
2021-07-11 00:23:33 +0700

    copy from pcs

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -2,54 +2,18 @@ import contracts from './contracts'
 import { FarmConfig, QuoteToken } from './types'
 
 const farms: FarmConfig[] = [
-  {
+  
+{
     pid: 0,
-    risk: 5,
     lpSymbol: 'BUSD-CFT LP',
     lpAddresses: {
       97: '0xa116a70d0a4955c8f749e4ef833d5bee2ce3119a',
       56: '',
     },
-    tokenSymbol: 'BNB',
-    tokenAddresses: {
-      97: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
-      56: '',
-    },
-    quoteTokenSymbol: QuoteToken.BNB,
-    quoteTokenAdresses: contracts.wbnb,
+    token: tokens.cft,
+    quoteToken: tokens.busd,
   },
-  {
-    pid: 1,
-    risk: 5,
-    lpSymbol: 'BNB-CFT LP',
-    lpAddresses: {
-      97: '0xeF7Ff8BBab806063E2C30Ac2C07bD6703B7AF421',
-      56: '',
-    },
-    tokenSymbol: 'BNB',
-    tokenAddresses: {
-      97: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
-      56: '',
-    },
-    quoteTokenSymbol: QuoteToken.BNB,
-    quoteTokenAdresses: contracts.wbnb,
-  },
-  {
-    pid: 2,
-    risk: 3,
-    lpSymbol: 'BNB-BUSD LP',
-    lpAddresses: {
-      97: '0xe0e92035077c39594793e61802a350347c320cf2',
-      56: '',
-    },
-    tokenSymbol: 'BUSD',
-    tokenAddresses: {
-      97: '0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7',
-      56: '',
-    },
-    quoteTokenSymbol: QuoteToken.BUSD,
-    quoteTokenAdresses: contracts.busd,
-  },
+
   
 
 ]
