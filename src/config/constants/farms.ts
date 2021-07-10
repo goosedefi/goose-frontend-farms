b9e2768b529ac664e7282ea8f59b2a3eecd4cf26
tuejul6 <87008121+tuejul6@users.noreply.github.com>
2021-07-10 13:59:48 +0700

    edit stuff

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -15,8 +15,8 @@ const farms: FarmConfig[] = [
       97: '0xDACee0C89E7d6CE4cE384C6de8B595343276CA3c',
       56: '',
     },
-    quoteTokenSymbol: QuoteToken.CFT,
-    quoteTokenAdresses: contracts.cft,
+    quoteTokenSymbol: QuoteToken.BUSD,
+    quoteTokenAdresses: contracts.busd,
   },
   {
     pid: 1,
--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'GooseFinance'
+            screenName: 'cz_binance'
           }}
           options={{
             height: '300',
