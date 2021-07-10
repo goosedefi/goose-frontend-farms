f3495deda2233fea8afee43ce94e9ad43d3a4b39
tuejul6 <87008121+tuejul6@users.noreply.github.com>
2021-07-11 00:07:18 +0700

    fix contract bnb-busd farms
    testing

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -10,13 +10,13 @@ const farms: FarmConfig[] = [
       97: '0xa116a70d0a4955c8f749e4ef833d5bee2ce3119a',
       56: '',
     },
-    tokenSymbol: 'BUSD',
+    tokenSymbol: 'BNB',
     tokenAddresses: {
-      97: '0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7',
+      97: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
       56: '',
     },
-    quoteTokenSymbol: QuoteToken.BUSD,
-    quoteTokenAdresses: contracts.busd,
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
   },
   {
     pid: 1,
@@ -28,7 +28,7 @@ const farms: FarmConfig[] = [
     },
     tokenSymbol: 'BNB',
     tokenAddresses: {
-      97: '0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7',
+      97: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
       56: '',
     },
     quoteTokenSymbol: QuoteToken.BNB,
@@ -42,13 +42,13 @@ const farms: FarmConfig[] = [
       97: '0xe0e92035077c39594793e61802a350347c320cf2',
       56: '',
     },
-    tokenSymbol: 'BNB',
+    tokenSymbol: 'BUSD',
     tokenAddresses: {
-      97: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
+      97: '0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7',
       56: '',
     },
-    quoteTokenSymbol: QuoteToken.BNB,
-    quoteTokenAdresses: contracts.wbnb,
+    quoteTokenSymbol: QuoteToken.BUSD,
+    quoteTokenAdresses: contracts.busd,
   },
   
 
