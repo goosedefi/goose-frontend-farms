bef649c7b87f503079050fdcc07b6bb151df6b26
tuejul6 <87008121+tuejul6@users.noreply.github.com>
2021-07-11 23:26:27 +0700

    edit to default goosedefi

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -25,7 +25,7 @@ const Menu = (props) => {
       setLang={setSelectedLanguage}
       cakePriceUsd={cakePriceUsd.toNumber()}
       links={config}
-      priceLink="https://www.coingecko.com/en/coins/goose-finance"
+      priceLink="#"
       {...props}
     />
   )
